38596ebeafb5b03d79028cde14e05288b24dd080
marko-fazz <marko.durasic@fazzfinancial.com>
2022-11-27 23:46:43 +0800

    PA-277: Remove commented out code

--- src/api/stressTest.go
+++ src/api/stressTest.go
@@ -7,20 +7,6 @@ import (
 )
 
 func stressTestDdosify() {
-	// run ddosify
-	// cmd := exec.Command("ddosify", "-c", "100", "-r", "100", "-m", "GET", "http://localhost:800")
-	// err := cmd.Start()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// // wait for ddosify to finish
-	// err = cmd.Wait()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// Sign POST /api/sign/{keyName} , json {"data": "hello"} , keyame = "k1"
-	// ddosify -m POST -t http://localhost:8000/api/sign/k1 -b "{\"data\": \"hello\"}" -l incremental
 
 	// Incremental load test
 	cmd := exec.Command("ddosify", "-m", "POST", "-t", "http://localhost:8000/api/sign/k1", "-b", "{\"data\": \"hello\"}", "-l", "incremental")
